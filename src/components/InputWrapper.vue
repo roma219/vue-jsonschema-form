0723f898344b22f624c08a06cfc17e13fadf7dde
roma219 <roma219@yandex.ru>
2019-09-18 19:45:38 +0300

    inputWrapper

---
+++ src/components/InputWrapper.vue
@@ -0,0 +1,17 @@
+<template>
+  <div class="input-wrapper">
+    <div class="input-wrapper-title">{{ title }}</div>
+    <slot/>
+  </div>
+</template>
+
+<script lang="ts">
+import { Component, Prop, Vue } from 'vue-property-decorator'
+
+@Component({
+  name: 'InputWrapper'
+})
+export default class InputWrapper extends Vue {
+  @Prop({ default: '' }) protected title!: string
+}
+</script>
