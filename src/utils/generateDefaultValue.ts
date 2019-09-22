c0c112a45f894ea74c402bb604518bb84e9cf170
Roman Sabirov <roma219@yandex.ru>
2019-09-22 11:41:16 +0300

    test commit

--- src/utils/generateDefaultValue.ts
+++ src/utils/generateDefaultValue.ts
@@ -3,6 +3,7 @@ import { IInnerSchema } from '@/types/jsonschema'
 export const generateDefaultValue = (schema: IInnerSchema) : { [key: string]: any } => {
   const value = {}
   if (schema.default) return schema.default
+
   if (schema.type === 'object') {
       Object.keys(schema.properties || {}).forEach(propName => {
         value[propName] = generateDefaultValue(schema.properties[propName])
