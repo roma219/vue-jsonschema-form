1fe7bcde9bacc9baf9f6507f0f9b5ca5800480c9
roma219 <roma219@yandex.ru>
2019-09-24 19:08:09 +0300

    vuepress update

--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -1,3 +1,22 @@
 module.exports = {
-  base: '/vue-jsonschema-form/'
+  base: '/vue-jsonschema-form/',
+  themeConfig: {
+    repo: 'roma219/vue-jsonschema-form',
+    nav: [
+      { text: 'Home', link: '/' },
+      { text: 'Guide', link: '/guide/' },
+      { text: 'External', link: 'https://google.com' }
+    ],
+    sidebar: [
+      {
+        title: 'Group 1',   // required
+        path: '/foo/',      // optional, which should be a absolute path.
+        collapsable: false, // optional, defaults to true
+        sidebarDepth: 1,    // optional, defaults to 1
+        children: [
+          '/'
+        ]
+      }
+    ]
+  }
 }
