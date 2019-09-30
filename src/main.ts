2c5a172cb5e9967bd22dd8c0e884ab7a61583fdc
roma219 <roma219@yandex.ru>
2019-09-30 18:52:08 +0300

    build:lib wip

--- src/main.ts
+++ src/main.ts
@@ -1,11 +1,5 @@
 import Vue from 'vue'
 import App from './App.vue'
-import { Input, InputNumber, Checkbox, FormItem } from 'element-ui'
-import 'element-ui/lib/theme-chalk/index.css'
-
-Vue.component(Input.name, Input)
-Vue.component(Checkbox.name, Checkbox)
-Vue.component(InputNumber.name, InputNumber)
 
 Vue.config.productionTip = false
 
