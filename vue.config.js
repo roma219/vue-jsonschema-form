40a45ecf622cbdd54feb9f0f1018b9d8cfcacd2e
roma219 <roma219@yandex.ru>
2019-10-02 15:13:45 +0300

    fix token

--- vue.config.js
+++ vue.config.js
@@ -2,6 +2,6 @@ const BundleAnalyzerPlugin = require('@bundle-analyzer/webpack-plugin')
 
 module.exports = {
   configureWebpack: {
-    plugins: [new BundleAnalyzerPlugin({ token: 'BUNDLE_ANALYZER_TOKEN=2bba2506df16839b5fa01e3a2e56594744ad1786' })]
+    plugins: [new BundleAnalyzerPlugin({ token: '2bba2506df16839b5fa01e3a2e56594744ad1786' })]
   }
 }
