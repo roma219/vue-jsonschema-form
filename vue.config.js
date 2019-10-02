b732cca57203f3e000af13929893a348eb910eb5
roma219 <roma219@yandex.ru>
2019-10-02 15:10:21 +0300

    webpack plugin bundle analyzer

---
+++ vue.config.js
@@ -0,0 +1,7 @@
+const BundleAnalyzerPlugin = require('@bundle-analyzer/webpack-plugin')
+
+module.exports = {
+  configureWebpack: {
+    plugins: [new BundleAnalyzerPlugin({ token: '2bba2506df16839b5fa01e3a2e56594744ad1786' })]
+  }
+}
