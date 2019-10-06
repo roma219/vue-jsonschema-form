0546d70a043634ac99fa2ad23538b65639ca615f
Roman Sabirov <roma219@yandex.ru>
2019-10-06 22:32:26 +0300

    externals

--- vue.config.js
+++ vue.config.js
@@ -2,7 +2,14 @@ const BundleAnalyzerPlugin = require('@bundle-analyzer/webpack-plugin')
 
 module.exports = {
   configureWebpack: {
-    plugins: [new BundleAnalyzerPlugin({ token: '2bba2506df16839b5fa01e3a2e56594744ad1786' })]
+    plugins: [new BundleAnalyzerPlugin({ token: '2bba2506df16839b5fa01e3a2e56594744ad1786' })],
+    externals: {
+      lodash: {
+        commonjs: 'lodash',
+        amd: 'lodash',
+        root: '_'
+      }
+    }
   },
   css: {
     extract: false
