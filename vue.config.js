ccc3cad1a913b03b8337082a1a40659e166f85b7
Roman Sabirov <roma219@yandex.ru>
2019-10-13 17:27:14 +0300

    test plugin

--- vue.config.js
+++ vue.config.js
@@ -1,8 +1,13 @@
 const BundleAnalyzerPlugin = require('@bundle-analyzer/webpack-plugin')
+const PacktrackerPlugin = require('@packtracker/webpack-plugin')
 
 module.exports = {
   configureWebpack: {
-    plugins: [new BundleAnalyzerPlugin({ token: '2bba2506df16839b5fa01e3a2e56594744ad1786' })],
+    plugins: [new BundleAnalyzerPlugin({ token: '2bba2506df16839b5fa01e3a2e56594744ad1786' }), new PacktrackerPlugin({
+      project_token: '80ba883f-9a5b-495d-8676-cf33c6d4970a',
+      upload: true,
+      fail_build: true
+    })],
     externals: {
       // 'vue': 'Vue'
     }
