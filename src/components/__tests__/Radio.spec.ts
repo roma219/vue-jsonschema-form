f7290dfafedcbdf9a385a417eb1dd785ebd61882
Roman Sabirov <roma219@yandex.ru>
2019-12-26 12:40:38 +0530

    Radio.vue unit-test

--- src/components/__tests__/Radio.spec.ts
+++ src/components/__tests__/Radio.spec.ts
@@ -6,4 +6,14 @@ describe('Radio component', () => {
     const wrapper = mount(Radio)
     expect(wrapper.isVueInstance()).toBeTruthy()
   })
+
+  it('should emit input event on change', () => {
+    const wrapper = mount(Radio, { propsData: { options:['1', '2', '3'], value: '2' } })
+    var event = new Event('input', {
+      bubbles: true,
+      cancelable: true,
+  });
+    wrapper.vm.$el.getElementsByTagName('input')[0].dispatchEvent(event)
+    expect(wrapper.emitted().input[0]).toEqual(['1'])
+  })
 })
