fc6d35b22bc1b1c8dfd5adee2a43ef9bd53434d4
Roman Sabirov <roma219@yandex.ru>
2019-12-26 12:11:52 +0530

    app.vue unit test

---
+++ src/JsonSchema/__mocks__/JsonSchema.vue
@@ -0,0 +1,10 @@
+<template>
+  <div>jsonschema</div>
+</template>
+
+<script lang="ts">
+import { Component, Vue } from 'vue-property-decorator'
+
+@Component({})
+export default class JsonSchema extends Vue {}
+</script>
---
+++ src/__tests__/App.spec.ts
@@ -0,0 +1,10 @@
+import App from '../App.vue'
+import { shallowMount } from '@vue/test-utils'
+jest.mock('../JsonSchema/JsonSchema.vue');
+
+describe('App.vuenpm', () => {
+  it('should be vue component', () => {
+    const wrapper = shallowMount(App)
+    expect(wrapper.isVueInstance()).toBeTruthy()
+  })
+})
