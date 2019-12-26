7d2862b6f1399b6eea862a537b64eca692130f72
Roman Sabirov <roma219@yandex.ru>
2019-12-26 12:28:52 +0530

    entry files unit tests

---
+++ src/__tests__/entry.spec.ts
@@ -0,0 +1,10 @@
+import entry from '../entry'
+import { shallowMount } from '@vue/test-utils'
+jest.mock('../JsonSchema/JsonSchema.vue');
+
+describe('entry.ts', () => {
+  it('should be a vue component', () => {
+    const wrapper = shallowMount(entry)
+    expect(wrapper.isVueInstance()).toBeTruthy()
+  })
+})
---
+++ src/__tests__/main.spec.ts
@@ -0,0 +1,12 @@
+jest.mock('../JsonSchema/JsonSchema.vue');
+
+describe('entry.ts', () => {
+  it('should render app in #app', () => {
+    document.body.innerHTML ='<div id="app"></div>';
+
+    require('../main')
+
+    const pElement = document.getElementsByClassName('schema');
+    expect(pElement.length).toBeTruthy();
+  })
+})
