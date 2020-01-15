7460f7b048ec648f97986dd97e42032498723faf
Roman Sabirov <roma219@yandex.ru>
2020-01-15 13:09:14 +0800

    test coverage for array operations

--- src/JsonSchema/__tests__/JsonSchema.spec.ts
+++ src/JsonSchema/__tests__/JsonSchema.spec.ts
@@ -14,7 +14,18 @@ const testSchema = {
     // z: { type: 'string', minLength: 1 },
     c: { type: 'string', enum: ['1', '2', '3'] },
     d: { type: 'array', items: { type: 'number' } },
-    e: { type: 'array', items: { type: 'object', properties: { f: { type: 'string', default: 'kek' } } } }
+    e: {
+      type: 'array',
+      items: {
+        type: 'object',
+        properties: {
+          f: {
+            type: 'string',
+            default: 'kek'
+          }
+        }
+      }
+    }
   }
 }
 
@@ -46,10 +57,22 @@ describe('JsonSchema.vue', () => {
     expect(wrapper.emitted().input[0]).toEqual([{ b: { c: 'aaa' } }])
   })
   it('creates empty object when inner property was changed', () => {
-    const wrapper = mount(JsonSchema, { propsData: { schema: testSchema, value: {} } })
+    const wrapper = mount(JsonSchema, { propsData: { schema: testSchema, value: { } } })
     const input = wrapper.find('input')
     ;(input.element as any).value = 'aaa'
     input.trigger('input')
     expect(wrapper.emitted().input[0]).toEqual([{ b: { c: 'aaa' } }])
+  })
+  it('correctly handles array element change', () => {
+    const wrapper = mount(JsonSchema, { propsData: { schema: testSchema, value: { e: [{ f: 'aaa' }, { f: 'bbb' }] } } })
+    const moveBtn = wrapper.find('button')
+    moveBtn.trigger('click')
+    expect(wrapper.emitted().input[0]).toEqual([ { e: [{ f: 'bbb' }, { f: 'aaa' }] } ])
+    const deleteBtn = wrapper.findAll('button').at(1)
+    deleteBtn.trigger('click')
+    expect(wrapper.emitted().input[1]).toEqual([ { e: [{ f: 'bbb' }] } ])
+    const addBtn = wrapper.find('.add-new-arr-item')
+    addBtn.trigger('click')
+    expect(wrapper.emitted().input[2]).toEqual([ { e: [{ f: 'aaa' }, { f: 'bbb' }, { f: 'kek' }] } ])
   })
 })
