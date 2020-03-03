78415dccc2b1306f1b6c117c295e90fe9eafd6fe
Roman Sabirov <roma219@yandex.ru>
2020-03-03 12:31:12 +0800

    cleanup & up version

--- src/utils/errorMessages.ts
+++ src/utils/errorMessages.ts
@@ -3,7 +3,7 @@ import { ErrorMessagesConfig } from '@/types'
 export const errorMessages : ErrorMessagesConfig = {
   minLength: (value: number) => `Minimal length: ${value}`,
   maxLength: (value: number) => `Maximum length: ${value}`,
-  minValue: (value: number) => `Minimal value: ${value}`,
+  minValue: (value: number) => `Minimum value: ${value}`,
   maxValue: (value: number) => `Maximum value: ${value}`,
   required: 'Field is required',
   default: 'Invalid Value'
